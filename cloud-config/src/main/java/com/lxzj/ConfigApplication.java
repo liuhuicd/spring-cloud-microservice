f7a0bc967540cae942d058204da86cf21c44e284
liuhuicd <liuhui.cn@hotmail.com>
2018-01-18 17:43:15 +0800

    Initial commit

---
+++ cloud-config/src/main/java/com/lxzj/ConfigApplication.java
@@ -0,0 +1,16 @@
+package com.lxzj;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
